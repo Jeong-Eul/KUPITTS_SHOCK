ffb155837d1fd6cbfb9aea3b6847d5f2ece3158c
Jeong-Eul <wjddmf0321@naver.com>
2024-07-30 16:06:54 +0900

    script

--- Fluids_Train_ML.sh
+++ Fluids_Train_ML.sh
@@ -1,16 +1,6 @@
 # Note! 
 # You will need to change the 2 data dirs below for each experiment attempt.
 
-python ./Models/Fluids_train.py \
-    --lr 0.0001 \
-    --hidden_layers 32 16 32 \
-    --batch_size 64 \
-    --num_epoch 100 \
-    --patience 4\
-    --ff_dropout 0.1 \
-    --ckpt_dir "./checkpoint" \
-    --log_dir "./log" \
-    --result_dir "./result" \
-    --mode "Inference" \
+python ./Models/Fluids_train_ML.py \
     --train_data_dir "C:\Users\DAHS\Desktop\Pitts_Shock\KUPITTS_SHOCK\Data shock\train_shock_dataset_state_cont_240721_Action16_Reward_Mean_VM.csv" \
     --valid_data_dir "C:\Users\DAHS\Desktop\Pitts_Shock\KUPITTS_SHOCK\Data shock\val_shock_dataset_state_cont_240721_Action16_Reward_Mean_VM.csv" 
